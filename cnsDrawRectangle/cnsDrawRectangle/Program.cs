decae20e693fce49318b4b9f97082f15f75a182c
sonyamrz <sofaelison@gmail.com>
2023-09-28 09:31:05 +0300

    28.09 lesson

--- cnsDrawRectangle/cnsDrawRectangle/Program.cs
+++ cnsDrawRectangle/cnsDrawRectangle/Program.cs
@@ -1,55 +1,91 @@
-﻿using System.Diagnostics.SymbolStore;
+﻿using System;
+using System.Collections.Generic;
 
-bool isContinue;
-
-do
+class SnakeGame
 {
-    int height, width;
-    char symbol;
-    string answer;
-    Console.Write("Введите высоту прямоугольника:-");
-    height = Convert.ToInt32(Console.ReadLine());
-    Console.Write("Введите ширину прямоугольника:-");
-    width = Convert.ToInt32(Console.ReadLine());
-    Console.Write("Введите один символ:-");
-    symbol = Convert.ToChar(Console.ReadLine());
+    static int width = 20; // Ширина игрового поля
+    static int height = 10; // Высота игрового поля
+    static int snakeX = 10; // Начальная позиция змейки по X
+    static int snakeY = 5; // Начальная позиция змейки по Y
+    static ConsoleColor snakeColor = ConsoleColor.Green; // Цвет змейки
+    static List<int> snakeTailX = new List<int>(); // Координаты хвоста змейки по X
+    static List<int> snakeTailY = new List<int>(); // Координаты хвоста змейки по Y
+    static int fruitX; // Позиция фрукта по X
+    static int fruitY; // Позиция фрукта по Y
+    static ConsoleColor fruitColor = ConsoleColor.Red; // Цвет фрукта
+    static int score = 0; // Счет игры
+    static bool gameOver = false; // Флаг окончания игры
 
-    Console.Write("Заполнить? [Y/N] - >");
-    answer = Console.ReadLine().ToUpper();
-    switch (answer)
+    static void Main()
     {
-        case "Y":
-            for (int i = 0; i <= height; i++)
+        Console.Title = "Змейка";
+        Console.CursorVisible = false;
+        Console.SetWindowSize(width + 2, height + 4);
+        Console.SetBufferSize(width + 2, height + 4);
+
+        // Инициализация змейки
+        snakeTailX.Add(snakeX);
+        snakeTailY.Add(snakeY);
+
+        // Генерация фрукта
+        GenerateFruit();
+
+        // Главный игровой цикл
+        while (!gameOver)
+        {
+            // Обработка нажатий клавиш
+            if (Console.KeyAvailable)
             {
-                for (int j = 1; j <= width; j++)
+                ConsoleKeyInfo key = Console.ReadKey(true);
+                switch (key.Key)
                 {
-                    Console.Write(symbol);
+                    case ConsoleKey.LeftArrow:
+                        MoveSnake(-1, 0);
+                        break;
+                    case ConsoleKey.RightArrow:
+                        MoveSnake(1, 0);
+                        break;
+                    case ConsoleKey.UpArrow:
+                        MoveSnake(0, -1);
+                        break;
+                    case ConsoleKey.DownArrow:
+                        MoveSnake(0, 1);
+                        break;
                 }
-                Console.WriteLine();
             }
-            break;
-            case "N":
-            for (int i = 1; i <= height; i++)
-            {
-                for (int j = 1; j <= width; j++)
-                {
-                    if ((i == 1 || i == height || j == 1 || j == width))
-                        Console.Write(symbol);
-                    else
-                        Console.Write(" ");
-                }
-                Console.WriteLine();
-            }
-            break;
+
+            // Движение змейки
+            MoveSnake(0, 0);
+
+            // Отрисовка игрового поля
+            DrawField();
+
+            // Задержка для обновления экрана
+            System.Threading.Thread.Sleep(100);
+        }
+
+        // Вывод сообщения о конце игры
+        Console.Clear();
+        Console.SetCursorPosition(width / 2 - 4, height / 2);
+        Console.WriteLine("Game Over");
+        Console.SetCursorPosition(width / 2 - 6, height / 2 + 1);
+        Console.WriteLine("Score: " + score);
+        Console.ReadLine();
     }
 
-    Console.WriteLine(" Повторить? [Y/N] -> ");
-    isContinue = Console.ReadLine()?.ToUpper() == "Y";
+    static void DrawField()
+    {
+        Console.Clear();
 
+        // Отрисовка верхней границы
+        for (int i = 0; i < width + 2; i++)
+        {
+            Console.SetCursorPosition(i, 0);
+            Console.Write("■");
+        }
 
-} while (isContinue);
-
-           
-
-       
-  
+        // Отрисовка боковых границ
+        for (int i = 0; i < height; i++)
+        {
+            Console.SetCursorPosition(0, i + 1);
+            Console.Write("■")
---
+++ cnsTuple/cnsTuple/Program.cs
@@ -0,0 +1,41 @@
+﻿// Кортеж
+
+using System.ComponentModel.DataAnnotations;
+
+var z1 = 5;
+var z2 = 6;
+//(1)
+var x1 = (2, 4);
+(int, int) x1a = (2, 4);
+var x1b = ("Миша", 19, 3.14);
+Console.WriteLine(x1.Item1);
+Console.WriteLine(x1.Item2);
+Console.WriteLine();
+
+//(2)
+(int min, int max) x2 =(2,4);
+Console.WriteLine(x2.min);
+Console.WriteLine(x2.max);
+Console.WriteLine();
+
+//(3)
+
+var x3 = (min: 2, max: 4);
+Console.WriteLine(x3.min);
+Console.WriteLine(x3.max);
+Console.WriteLine();
+
+//(4)
+
+var (min, max) = (2, 4);
+Console.WriteLine(min);
+Console.WriteLine(max);
+Console.WriteLine();
+
+//(5)
+var x5 = GetX5();
+Console.WriteLine(x5.Item1);
+
+(int, int) GetX5() => (2, 4);
+(int min, int max) GetX6() => (2, 4);
+(int min, int max) GetX7( int v, (int a, int b) ab) => (v +ab.a, v+ab.b);

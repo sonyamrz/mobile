41e067405aab25ccf25290d511b4fafdaa6b25e1
sonyamrz <sofaelison@gmail.com>
2023-10-12 11:32:05 +0300

    new 12.10.2023

---
+++ cnsQueue/cnsQueue/Program.cs
@@ -0,0 +1,28 @@
+﻿// очередь Queue
+
+// обычная очередь
+using System.Collections;
+
+Queue queue = new();
+queue.Enqueue(100500);
+queue.Enqueue("hello");
+queue.Enqueue(3.14);
+Console.WriteLine($"Смотрим -  {queue.Peek()}");
+
+while (queue.Count > 0)
+{
+    Console.WriteLine(queue.Dequeue());
+}
+
+// обобщенная очередь
+Queue<int> numbers = new();
+numbers.Enqueue(1);
+numbers.Enqueue(2);
+numbers.Enqueue(3);
+numbers.Enqueue(4);
+Console.WriteLine($"Смотрим -  {numbers.Sum()}");
+
+while (numbers.Count > 0)
+{
+    Console.WriteLine(numbers.Dequeue());
+}
---
+++ wfaGameTrainerAccount/wfaGameTrainerAccount/Game.cs
@@ -0,0 +1,54 @@
+﻿
+using System.Diagnostics.Eventing.Reader;
+
+namespace wfaGameTrainerAccount
+{
+    internal class Game
+    {
+        private Random rnd = new();
+        public int CountCorrect { get; private set; }
+        public int CountWrong { get; private set; }
+        public string CodeText { get; private set; }
+
+        private bool answerCorrect;
+
+        public event EventHandler Change;
+
+        public void DoReset()
+        {
+            CountCorrect = 0;
+            CountWrong = 0;
+        }
+
+        private void DoContinue()
+        {
+            //CodeText = "11+22=33";
+           // answerCorrect = true;
+
+            int xValue1 = rnd.Next(20);
+            int xValue2 = rnd.Next(20);
+            int xResult = xValue1+xValue2;
+
+            int xResultNew = xResult;
+
+            if (rnd.Next(2) == 1)
+                xResultNew += rnd.Next(1, 7) * (rnd.Next(2) == 1 ? 1 : -1);
+                CodeText = $"{xValue1} + {xValue2} = {xResultNew}";
+                answerCorrect = (xResultNew == xResult);
+            Change?.Invoke(this, EventArgs.Empty);
+        }
+
+        public void DoAnswer (bool v)
+        {
+            if (v ==  answerCorrect)
+            
+                CountCorrect++;
+                else 
+                    CountWrong++;
+                DoContinue();
+            
+        }
+
+
+    }
+}
